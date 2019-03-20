ae55743492322e74f9c941ab8bc15af9a31f876e
Ford Hurley <ford.hurley@gmail.com>
2019-03-20 07:54:17 -0400

    Package up websocket logic

--- build/bundle.js
+++ build/bundle.js
@@ -340,7 +340,72 @@ var shade = (function (exports) {
       });
   }
 
+  class WebSocket {
+      constructor(path, url) {
+          this.path = path;
+
+          this.ws = new window.WebSocket(url);
+          this.ws.onopen = (event) => {
+              console.log("connected:", event);
+              this.sendWatch(this.path);
+          };
+          this.ws.onclose = (event) => {
+              console.log("close:", event);
+          };
+          this.ws.onerror = (event) => {
+              console.log("error:", event);
+          };
+          this.ws.onmessage = (event) => {
+              console.log("message:", event.data);
+              this.handleMessage(JSON.parse(event.data));
+          };
+
+          this.eventHandlers = {};
+      }
+
+      handleMessage(msg) {
+          switch (msg.command) {
+          case "changed":
+              if (msg.path !== this.path) {
+                  return
+              }
+              this.getEventListeners("changed").forEach((handler) => {
+                  handler(msg.path);
+              });
+              break
+          default:
+              console.warn("unknown command:", msg.command);
+          }
+      }
+
+      addEventListener(name, callback) {
+          this.getEventListeners(name).push(callback);
+      }
+
+      getEventListeners(name) {
+          let handlers = this.eventHandlers[name];
+          if (!handlers) {
+              handlers = [];
+          }
+          this.eventHandlers[name] = handlers;
+          return handlers
+      }
+
+      sendWatch(path) {
+          this.ws.send(JSON.stringify({
+              command: "watch",
+              path,
+          }));
+      }
+
+      // callback called with a string path arg.
+      onChanged(callback) {
+          this.addEventListener("changed", callback);
+      }
+  }
+
   exports.Shader = Shader;
+  exports.WebSocket = WebSocket;
 
   return exports;
 
---
+++ src/shader.js
@@ -0,0 +1,117 @@
+import {ShaderCanvas} from "shader-canvas"
+
+export class Shader {
+    constructor(containerEl) {
+        this.canvas = new ShaderCanvas()
+        containerEl.appendChild(this.canvas.domElement)
+
+        this.animate = this.animate.bind(this)
+        this.mousemove = this.mousemove.bind(this)
+
+        this.canvas.setSize(400, 400)
+        this.setShader(`
+            precision mediump float;
+            void main() {
+                gl_FragColor = vec4(0.8, 0.8, 0.8, 1.0);
+            }
+        `)
+    }
+
+    load(url) {
+        console.log("load:", url)
+        const req = new Request(url)
+        req.headers.set("accept", "application/x-shader")
+        fetch(req).then((res) => {
+            return res.json()
+        }).then((data) => {
+            if (data.compiledSource) {
+                this.setShader(data.compiledSource)
+            } else {
+                this.setShader(data.source)
+            }
+        }).catch((err) => {
+            console.error(err)
+        })
+    }
+
+    setShader(source) {
+        this.source = source
+        this.canvas.setShader(this.source)
+
+        if (testUniform("vec2", "u_resolution", this.source)) {
+            this.canvas.setUniform("u_resolution", this.canvas.getResolution())
+        }
+
+        this.isAnimated = testUniform("float", "u_time", this.source)
+
+        this.canvas.domElement.removeEventListener("mousemove", this.mousemove)
+        if (testUniform("vec2", "u_mouse", this.source)) {
+            this.canvas.domElement.addEventListener("mousemove", this.mousemove)
+        }
+
+        cancelAnimationFrame(this.frameRequest)
+
+        const textureDirectives = parseTextureDirectives(this.source)
+        Promise.all(textureDirectives.map(({filePath, name}) => {
+            return loadImage(filePath).then((img) => {
+                this.canvas.setTexture(name, img)
+            })
+        })).then(() => {
+            if (this.isAnimated) {
+                this.frameRequest = requestAnimationFrame(this.animate)
+            } else {
+                this.canvas.render()
+            }
+        }).catch((reason) => {
+            console.error(reason);
+        })
+    }
+
+    animate(timestamp) {
+        this.frameRequest = requestAnimationFrame(this.animate)
+        this.canvas.setUniform("u_time", timestamp / 1000)
+        this.canvas.render()
+    }
+
+    mousemove(e) {
+        const mouse = [
+            e.offsetX / this.canvas.width,
+            1 - (e.offsetY / this.canvas.height),
+        ]
+        this.canvas.setUniform("u_mouse", mouse);
+
+        if (!this.isAnimated) {
+            this.canvas.render()
+        }
+    }
+}
+
+function testUniform(type, name, source) {
+    const re = new RegExp(`^\\s*uniform\\s+${type}\\s+${name}`, "m");
+    return re.test(source);
+}
+
+function parseTextureDirectives(source) {
+    // Looking for lines of the form:
+    // uniform sampler2D foo; // ../textures/foo.png
+    const re = /^\s*uniform\s+sampler2D\s+(\S+)\s*;\s*\/\/\s*(\S+)\s*$/gm;
+    const out = [];
+    let match = re.exec(source);
+    while (match !== null) {
+        const name = match[1];
+        const filePath = match[2];
+        out.push({name, filePath});
+        match = re.exec(source);
+    }
+    return out;
+}
+
+function loadImage(src) {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => { resolve(img); };
+        img.onerror = reject;
+        img.onabort = reject;
+    });
+}
