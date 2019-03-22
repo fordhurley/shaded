41ad08e463f17e431fa5c5897866cc81537cc72f
Ford Hurley <ford.hurley@gmail.com>
2019-03-22 17:47:35 -0400

    Add a gradient in the time example

--- examples/time.glsl
+++ examples/time.glsl
@@ -1,8 +1,10 @@
 precision mediump float;
 
 uniform float u_time;
+uniform vec2 u_resolution;
 
 void main() {
+    vec2 uv = gl_FragCoord.xy / u_resolution;
     float t = sin(u_time) * 0.5 + 0.5;
-    gl_FragColor = vec4(t, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(uv.x, t, uv.y, 1.0);
 }
